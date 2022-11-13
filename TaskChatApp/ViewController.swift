9a083255febe2fe438e27b9e65332152ae7a4b88
卓馬純之介 <juntakuma27@i.softbank.jp>
2022-11-13 11:16:22 +0900

    Initial Commit

---
+++ TaskChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskChatApp
+//
+//  Created by 卓馬純之介 on 2022/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
